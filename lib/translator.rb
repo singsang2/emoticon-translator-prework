7e9038a5a510ad274d3f190217254e0bd2c9db8c
Sung Bae <singsang2@gmail.com>
2018-10-19 04:09:47 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,8 +11,9 @@ def load_library(path="./lib/emoticons.yml")
   new_hash
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(path, emot)
+  dictionary = load_library(path)
+  dictionary['get_emoticon'][emot]
 end
 
 def get_english_meaning
